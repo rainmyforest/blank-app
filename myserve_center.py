88459ffc81c7eb071a71c24fb10f91cc83fef0e7
rainmyforest <117068652+rainmyforest@users.noreply.github.com>
2024-11-15 12:44:22 +0800

    Update myserve_center.py

--- myserve_center.py
+++ myserve_center.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 # streamlit run myserve_center.py
 # 使用st.image来展示图片
-st.image("Data/image.png", use_column_width=True, caption="Your Image Caption")
+st.image("Data/image.jpg", use_column_width=True, caption="Your Image Caption")
